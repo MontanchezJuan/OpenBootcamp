adce9c8b371c0a12ebcd67bfc63c72fa8aea71ae
MontanchezJuan <121134352+MontanchezJuan@users.noreply.github.com>
2023-02-25 14:06:56 -0500

    Update fechas.js

--- JavaScript-Basico/Tema-7/Ejercicio-3/fechas.js
+++ JavaScript-Basico/Tema-7/Ejercicio-3/fechas.js
@@ -1,1 +1,11 @@
-a
+const hoy = new Date()
+
+const birthdate = new Date(2003,3,14)
+
+const comprobante = hoy > birthdate
+
+const day = birthdate.getDate()
+
+const month = birthdate.getMonth() + 1
+
+const year = birthdate.getFullYear()
