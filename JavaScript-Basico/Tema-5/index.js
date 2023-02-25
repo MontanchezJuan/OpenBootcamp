02ec806c43f58926bd95fda051ba1b1d7ff3610d
MontanchezJuan <121134352+MontanchezJuan@users.noreply.github.com>
2023-02-25 13:19:00 -0500

    Create index.js

---
+++ JavaScript-Basico/Tema-5/index.js
@@ -0,0 +1,10 @@
+let alturaCm = 160
+
+let alturaM = alturaCm / 100
+
+let pesoKg = 60.0
+
+let alturaRedondeada = Math.ceil(alturaM)
+let pesoRedondeado = Math.ceil(pesoKg)
+
+let max = Number.MAX_VALUE + 1
