550753137a840f2774baf326ee04bccd02fe8f8c
MontanchezJuan <121134352+MontanchezJuan@users.noreply.github.com>
2023-02-25 12:14:53 -0500

    Create Persona.java

---
+++ Introduccion/Tema-9/Persona.java
@@ -0,0 +1,33 @@
+class Persona {
+    // Crea una clase Persona con las siguientes variables:
+    // La edad
+    // El nombre
+    // El teléfono
+    protected int edad;
+    protected String nombre;
+    protected String telefono;
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return this.telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
