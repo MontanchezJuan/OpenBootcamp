b5fabbef49153a767ec6381c96784d8a10894f84
MontanchezJuan <121134352+MontanchezJuan@users.noreply.github.com>
2023-02-25 11:51:16 -0500

    Create Cliente.java

---
+++ Introduccion/Ejericicios-tema-9-Introduccion/Cliente.java
@@ -0,0 +1,11 @@
+public class Cliente extends Persona {
+    private boolean credito = false;
+
+    public boolean getCredito() {
+        return this.credito;
+    }
+
+    public void setCredito(boolean credito) {
+        this.credito = credito;
+    }
+}
