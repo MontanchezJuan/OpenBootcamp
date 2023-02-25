5546a8fdea8cea8fc949032fee2cbc0582e51b4d
MontanchezJuan <121134352+MontanchezJuan@users.noreply.github.com>
2023-02-25 13:48:38 -0500

    Update objetos.js

--- JavaScript-Basico/Tema-7/Ejercicio-2/objetos.js
+++ JavaScript-Basico/Tema-7/Ejercicio-2/objetos.js
@@ -1,1 +1,26 @@
-a
+const me = {
+    name: 'Juan',
+    last_name: 'Montanchez',
+    age: 19,
+    altura: '1.60 m',
+    isDeveloper: true}
+
+let edad = me.age
+
+const santi = {
+    name: 'Santiago',
+    last_name: 'Bedoya',
+    age: 21,
+    altura: '1.68 m',
+    isDeveloper: true}
+
+const marco = {
+    name: 'Marco',
+    last_name: 'Zuñiga',
+    age: 19,
+    altura: '1.80 m',
+    isDeveloper: true}
+
+const list = [me,santi,marco]
+
+const orderList = list.sort((a,b)=> a.age - b.age)
