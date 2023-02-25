d88ec57b5d39e8b9fcd2d9cc63a19b63d34b4851
MontanchezJuan <121134352+MontanchezJuan@users.noreply.github.com>
2023-02-25 13:14:42 -0500

    Create me.js

---
+++ JavaScript-Basico/Tema-2/me.js
@@ -0,0 +1,9 @@
+const favoriteBook = {
+    titulo: "Conversaciones cruciales",
+    autor: "Kerry Patterson",
+    fecha: new Date(2001,8,16),
+    url: `https://www.amazon.com/-/es/Kerry-Patterson/dp/8492921374`
+}
+const personalData = ["Juan Montanchez",19,true,new Date(2003,3,14),favoriteBook]
+
+console.log(personalData);
